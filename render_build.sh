ee28f0aaf39d94740fa7cd8013f98f58acbaf9bb
Jesse Taylor <jesse.taylor.2806@gmail.com>
2025-05-16 16:43:23 +1000

    updated bash so don't have to install flutter each time

--- render_build.sh
+++ render_build.sh
@@ -2,7 +2,13 @@
 set -o errexit
 
 # Install Flutter if needed (Render uses cached builds, but this ensures a clean install)
-git clone https://github.com/flutter/flutter.git -b stable
+# git clone https://github.com/flutter/flutter.git -b stable
+
+# Check if Flutter is already cloned (cached between builds)
+if [ ! -d "$HOME/flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable "$HOME/flutter"
+fi
+
 export PATH="$PATH:`pwd`/flutter/bin"
 
 # Enable web
