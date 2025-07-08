03d53a861cf309446a2d4c0585ad93f0e0588add
Jesse Taylor <jesse.taylor.2806@gmail.com>
2025-07-08 11:40:25 +1000

    test url launcher

--- lib/Store/view_models/store_view_model.dart
+++ lib/Store/view_models/store_view_model.dart
@@ -5,6 +5,7 @@ import 'package:aiso/Store/widgets/store_desktop.dart';
 import 'package:aiso/Store/widgets/verify_email_dialog.dart';
 import 'package:aiso/models/purchase_enum.dart';
 import 'package:aiso/services/store_service_supabase.dart';
+import 'package:aiso/services/url_launcher_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -18,6 +19,7 @@ import 'package:web/web.dart' as web;
 class StoreViewModel extends ChangeNotifier {
 
   final StoreServiceSupabase _storeService = StoreServiceSupabase();
+  // final UrlLauncherService _urlLauncherService = UrlLauncherService();
 
   bool isLoading = false;
   String? errorMessage;
@@ -45,6 +47,10 @@ class StoreViewModel extends ChangeNotifier {
         ],
         callToAction: 'Buy Now',
         onPressed: (context) => handleProductAction(context, ProductType.PURCHASE)
+        // onPressed: (context) => UrlLauncherService.launchFromAsyncSource(() {
+        //   return handleProductAction(context, ProductType.PURCHASE);
+        // }),
+
       ),
       Product(
         id: '2',
@@ -63,7 +69,10 @@ class StoreViewModel extends ChangeNotifier {
           ProductInclusion(isIncluded: true, description: 'Automated monthly report updates'),
         ],
         callToAction: 'Subscribe',
-        onPressed: (context) => handleProductAction(context, ProductType.SUBSCRIBE_MONTHLY),
+        // onPressed: (context) => handleProductAction(context, ProductType.SUBSCRIBE_MONTHLY),
+        onPressed: (context) => UrlLauncherService.launchFromAsyncSource(() {
+          return handleProductAction(context, ProductType.SUBSCRIBE_MONTHLY);
+        }),
       ),
       Product(
         id: '3',
@@ -82,7 +91,10 @@ class StoreViewModel extends ChangeNotifier {
           ProductInclusion(isIncluded: true, description: 'Automated monthly report updates'),
         ],
         callToAction: 'Subscribe',
-        onPressed: (context) => handleProductAction(context, ProductType.SUBSCRIBE_YEARLY),
+        // onPressed: (context) => handleProductAction(context, ProductType.SUBSCRIBE_YEARLY),
+        onPressed: (context) => UrlLauncherService.launchFromAsyncSource(() {
+          return handleProductAction(context, ProductType.SUBSCRIBE_YEARLY);
+        }),
 
       ),
     ];
@@ -105,67 +117,103 @@ class StoreViewModel extends ChangeNotifier {
     // - sending logs to remote error tracking services
   }
 
-  void handleProductAction(BuildContext context, ProductType productType) {
+  // Future<String?> handleProductAction(BuildContext context, ProductType productType) async {
+  //   // This should return a URL (or null on error)
+  //   final url = await _storeService.generateCheckoutUrl(productType, reportId: '');
+  //   return url;
+  // }
+
+
+  // void handleProductAction(BuildContext context, ProductType productType) {
+  //   final user = Supabase.instance.client.auth.currentUser;
+
+  //   if (user == null) {
+  //     showSignInDialog(context);
+  //     return;
+  //   }
+
+  //   if (user.emailConfirmedAt == null) {
+  //     showVerifyEmailDialog(context);
+  //     return;
+  //   }
+
+  //   switch (productType) {
+  //     case ProductType.PURCHASE:
+  //       showBuyReportDialog(context);
+  //       break;
+
+  //     case ProductType.SUBSCRIBE_MONTHLY:
+  //     case ProductType.SUBSCRIBE_YEARLY:
+  //       launchCheckoutUrl(productType);
+  //       break;
+
+  //     default:
+  //       debugPrint("DEBUG: Unhandled product type in handleProductAction: $productType");
+  //   }
+  // }
+
+  Future<String?> handleProductAction(BuildContext context, ProductType productType) async {
     final user = Supabase.instance.client.auth.currentUser;
 
     if (user == null) {
       showSignInDialog(context);
-      return;
+      return null;
     }
 
     if (user.emailConfirmedAt == null) {
       showVerifyEmailDialog(context);
-      return;
+      return null;
     }
 
     switch (productType) {
       case ProductType.PURCHASE:
-        showBuyReportDialog(context);
-        break;
+        showBuyReportDialog(context); // UI action, not a checkout URL
+        return null;
 
       case ProductType.SUBSCRIBE_MONTHLY:
       case ProductType.SUBSCRIBE_YEARLY:
-        launchCheckoutUrl(productType);
-        break;
+        return _storeService.generateCheckoutUrl(productType, reportId: '');
 
       default:
         debugPrint("DEBUG: Unhandled product type in handleProductAction: $productType");
+        return null;
     }
   }
 
 
 
-  Future<void> launchCheckoutUrl(ProductType productType) async {
 
-    try {
-      final String? url = await _storeService.generateCheckoutUrl(productType, reportId: '');
+  // Future<void> launchCheckoutUrl(ProductType productType) async {
 
-      if (url == null) {
-        debugPrint('Checkout URL is null');
-        return;
-      }
+  //   try {
+  //     final String? url = await _storeService.generateCheckoutUrl(productType, reportId: '');
 
-      final Uri uri = Uri.parse(url);
+  //     if (url == null) {
+  //       debugPrint('Checkout URL is null');
+  //       return;
+  //     }
 
-      if (kIsWeb) {
-        // Open in new browser tab
-        // await launchUrl(uri, webOnlyWindowName: '_blank',);
-        web.window.open(url, '_blank');
-      } else {
-        // Open in external browser
-        if (await canLaunchUrl(uri)) {
-          await launchUrl(uri, mode: LaunchMode.externalApplication);
-        } else {
-          debugPrint('Could not launch $url');
-        }
-      }
-    } catch (e) {
-      debugPrint('launchCheckoutUrl error: $e');
-    } finally {
-      notifyListeners();
-    }
+  //     final Uri uri = Uri.parse(url);
 
-  }
+  //     if (kIsWeb) {
+  //       // Open in new browser tab
+  //       // await launchUrl(uri, webOnlyWindowName: '_blank',);
+  //       web.window.open(url, '_blank');
+  //     } else {
+  //       // Open in external browser
+  //       if (await canLaunchUrl(uri)) {
+  //         await launchUrl(uri, mode: LaunchMode.externalApplication);
+  //       } else {
+  //         debugPrint('Could not launch $url');
+  //       }
+  //     }
+  //   } catch (e) {
+  //     debugPrint('launchCheckoutUrl error: $e');
+  //   } finally {
+  //     notifyListeners();
+  //   }
+
+  // }
 
 
 }
